6cfc860b35f07761f86bf74e68be952e3127586a
Shivachauhan17 <shivanbd2019@gmail.com>
2024-03-06 18:24:06 +0530

    local peer connection worked

--- peerConnectionObjectLocally/js/localPeerConnection.js
+++ peerConnectionObjectLocally/js/localPeerConnection.js
@@ -26,6 +26,7 @@ function successCallback(stream){
     log("received local stream")
 
     localVideo.srcObject=stream
+    localStream=stream
     callButton.disabled=false
 }   
 
@@ -37,7 +38,7 @@ function start(){
     navigator.mediaDevices.webkitGetUserMedia ||
     navigator.mediaDevices.mozGetUserMedia;
 
-    navigator.mediaDevices.getUserMedia({audio:true,video:true})
+    navigator.mediaDevices.getUserMedia({audio:false,video:true})
         .then(successCallback)
         .catch(log)   
 }
@@ -50,16 +51,16 @@ function call(){
 
     var server=null;
 
-    localPeerConnection=RTCPeerConnection(server)
+    localPeerConnection=new RTCPeerConnection(server)
     log("Created local peer connection object localPeerConnection");
     
     localPeerConnection.onicecandidate = gotLocalIceCandidate;
 
-    remotePeerConnection = new RTCPeerConnection(servers);
+    remotePeerConnection = new RTCPeerConnection(server);
     log("Created remote peer connection object remotePeerConnection");
 
     remotePeerConnection.onicecandidate = gotRemoteIceCandidate;
-    remotePeerConnection.ontrack = gotRemoteStream;
+    remotePeerConnection.onaddstream = gotRemoteStream;
 
     localStream.getTracks().forEach(track => localPeerConnection.addTrack(track, localStream));
     log("Added localStream to localPeerConnection");
@@ -67,12 +68,12 @@ function call(){
 }
 
 function onSignalingError(error){
-    console.log('failed to create signaling message:',+ error.name)
+    console.log('failed to create signaling message:\n'+ error.name)
 }
 
 function gotLocalDescription(description){
     localPeerConnection.setLocalDescription(description)
-    log("offer from localPeerConnection",description.sdp);
+    log("offer from localPeerConnection \n"+description.sdp);
 
     remotePeerConnection.setRemoteDescription(description);
 
@@ -80,6 +81,7 @@ function gotLocalDescription(description){
 }
 
 function gotRemoteDescription(description){
+    log("got remote description\n"+description.sdp)
     remotePeerConnection.setLocalDescription(description)
     localPeerConnection.setRemoteDescription(description)
 }
@@ -102,17 +104,22 @@ function hangup(){
 
 function gotRemoteStream(event){
     remoteVideo.srcObject=event.stream
-    log("Received remote stream");
+    log("Received remote stream:\n"+event.stream);
 
 }
 
 function gotLocalIceCandidate(event){
-    remotePeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate))
-    log("Local ICE candidate: \n" + event.candidate.candidate)
+    if(event.candidate){
+        remotePeerConnection.addIceCandidate(event.candidate)
+        log("Local ICE candidate: \n" + event.candidate.candidate)
+    }
+    
 }
 
 function gotRemoteIceCandidate(event){
-    localPeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate))
-    log("Remote ICE candidate: \n " + event.candidate.candidate)
+    if(event.candidate){
+        localPeerConnection.addIceCandidate(event.candidate)
+        log("Remote ICE candidate: \n " + event.candidate.candidate)
+    }
 }
 
